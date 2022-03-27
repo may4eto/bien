eba60f28e4d40f2f566bd0ddaae03e480da099c4
Maya Mircheva <may4eto@hotmail.com>
2022-03-27 16:40:30 +0200

    changed AWS security key

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider:              'AWS',                        # required
-      aws_access_key_id:     'AKIA4CNFWQNEYOWJ7L44',                        # required unless using use_iam_profile
-      aws_secret_access_key: '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',                        # required unless using use_iam_profile
+      aws_access_key_id:     'AKIA4CNFWQNEQDVBCVX6',                        # required unless using use_iam_profile
+      aws_secret_access_key: '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',                        # required unless using use_iam_profile
       region:                'eu-central-1',                 
     }
     config.fog_directory  = 'bien-restaurant-reviews'                                     
