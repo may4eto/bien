9dce59c02480134a3640c9eadc2522e2dd68a2de
Maya Mircheva <may4eto@hotmail.com>
2022-05-02 18:14:03 +0200

    change to the aws credentials

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider:              'AWS',                        # required
-      aws_access_key_id:     Rails.application.credentials.aws[:access_key_id],                        # required unless using use_iam_profile
-      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],                        # required unless using use_iam_profile
+      aws_access_key_id:     Rails.application.credentials.dig(:aws, :access_key_id),                        # required unless using use_iam_profile
+      aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),                        # required unless using use_iam_profile
       region:                'eu-central-1',                 
     }
     config.fog_directory  = 'bien-restaurant-reviews'                                     
