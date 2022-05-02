cdf46ee911b6d1b89828cc090ac55e01fa609673
Maya Mircheva <may4eto@hotmail.com>
2022-05-02 13:05:13 +0200

    :added aws keys to the credentails file

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider:              'AWS',                        # required
-      aws_access_key_id:     'AKIA4CNFWQNEQDVBCVX6',                        # required unless using use_iam_profile
-      aws_secret_access_key: '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',                        # required unless using use_iam_profile
+      aws_access_key_id:     '<%= Rails.application.credentials.aws[:access_key_id] %>',                        # required unless using use_iam_profile
+      aws_secret_access_key: '<%= Rails.application.credentials.aws[:secret_access_key] %>',                        # required unless using use_iam_profile
       region:                'eu-central-1',                 
     }
     config.fog_directory  = 'bien-restaurant-reviews'                                     
